73a316319aa504e357900a1021fc216d52ae0231
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-06-25 02:07:15 -0300

    feat: add import of product modules

--- src/e_commerce_api/__init__.py
+++ src/e_commerce_api/__init__.py
@@ -1,3 +1,7 @@
-from .app import app, main
+from flask import Blueprint
+from .app import app, main, set_blueprint
 from .database import db
-from .routes import get_root
+from .routes import product_blueprint, get_root, post_product, get_products, get_product_by_id, delete_product_by_id, patch_product_by_id
+from .models import Product
+
+app.register_blueprint(product_blueprint, url_prefix='/api/products')
--- src/e_commerce_api/app.py
+++ src/e_commerce_api/app.py
@@ -1,6 +1,9 @@
-from flask import Flask
+from flask import Flask, Blueprint
 
 app = Flask(__name__)
+
+def set_blueprint(blueprint_name: str):
+    return Blueprint(blueprint_name, __name__)
 
 def main():
     app.run(debug=True, host="0.0.0.0")
