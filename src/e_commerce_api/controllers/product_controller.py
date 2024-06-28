72576660c86ee8c7377d195c9a13dbd400106a9d
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-06-27 22:46:08 -0300

    refactor: add product controllers

--- src/e_commerce_api/controllers/product_controller.py
+++ src/e_commerce_api/controllers/product_controller.py
@@ -1,5 +1,5 @@
 from flask import request, make_response, jsonify
-from ..use_cases.factories import make_create_product_use_case, make_list_products_use_case, make_fetch_product_by_id_use_case, make_remove_product_by_id_use_case, make_update_product_by_id_use_case
+from ..use_cases import make_create_product_use_case, make_list_products_use_case, make_fetch_product_by_id_use_case, make_remove_product_by_id_use_case, make_update_product_by_id_use_case
 
 def create_product():
     product_data = request.json
