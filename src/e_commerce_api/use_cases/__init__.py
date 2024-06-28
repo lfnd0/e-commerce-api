16e97fb509fa53133c9951b53eee6a32ecc59fa8
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-06-27 22:48:59 -0300

    refactor: import product factories

--- src/e_commerce_api/use_cases/__init__.py
+++ src/e_commerce_api/use_cases/__init__.py
@@ -1,5 +1,1 @@
-from .create_product_use_case import CreateProductUseCase
-from .list_products_use_case import ListProductsUseCase
-from .fetch_product_by_id_use_case import FetchProductByIdUseCase
-from .remove_product_by_id_use_case import RemoveProductByIdUseCase
-from .update_product_by_id_use_case import UpdateProductByIdUseCase
+from .products.factories import make_create_product_use_case, make_list_products_use_case, make_fetch_product_by_id_use_case, make_remove_product_by_id_use_case, make_update_product_by_id_use_case
