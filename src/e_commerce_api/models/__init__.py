432a24f2619860000868e3c4875fa87eb5d97a81
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-06-28 23:33:18 -0300

    feat: add user model

--- src/e_commerce_api/models/__init__.py
+++ src/e_commerce_api/models/__init__.py
@@ -1,1 +1,2 @@
 from .product import Product, ProductSchema, product_schema, products_schema
+from .user import User, UserSchema, user_schema, users_schema
---
+++ src/e_commerce_api/models/user.py
@@ -0,0 +1,17 @@
+from ..database import db, ma
+from datetime import datetime
+from flask_login import UserMixin
+
+class User(db.Model, UserMixin):
+    id = id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False, unique=True)
+    password = db.Column(db.String(100), nullable=True)
+    created_at = db.Column(db.DateTime, default=datetime.now())
+    updated_at = db.Column(db.DateTime, nullable=True)
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ("id", "username")
+
+user_schema = UserSchema()
+users_schema = UserSchema(many=True)
