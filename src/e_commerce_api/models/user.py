7baf193ba801ec75959ba4674b78b0f22ac87a6a
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-07-05 23:12:42 -0300

    refactor: change password field to password_hash in user model

--- src/e_commerce_api/models/user.py
+++ src/e_commerce_api/models/user.py
@@ -5,7 +5,7 @@ from flask_login import UserMixin
 class User(db.Model, UserMixin):
     id = id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), nullable=False, unique=True)
-    password = db.Column(db.String(100), nullable=True)
+    password_hash = db.Column(db.String(100), nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.now())
     updated_at = db.Column(db.DateTime, nullable=True)
 
