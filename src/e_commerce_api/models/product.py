208f227cbb8735ac2748fe57d7cb679102338551
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-07-10 20:46:01 -0300

    feat: add owner to product model

--- src/e_commerce_api/models/product.py
+++ src/e_commerce_api/models/product.py
@@ -1,17 +1,35 @@
-from ..database import db, ma
 from datetime import datetime
+from ..database import db, ma
+from .user import UserSchema
+
 
 class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Float, nullable=False)
     description = db.Column(db.Text, nullable=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now())
     updated_at = db.Column(db.DateTime, nullable=True)
 
+    owner = db.relationship("User", backref=db.backref("products", lazy=True))
+
+
 class ProductSchema(ma.Schema):
+    owner = ma.Nested(UserSchema)
+
     class Meta:
-        fields = ("id", "name", "price", "description", "created_at", "updated_at")
+        fields = (
+            "id",
+            "name",
+            "price",
+            "description",
+            "owner_id",
+            "created_at",
+            "updated_at",
+            "owner",
+        )
+
 
 product_schema = ProductSchema()
 products_schema = ProductSchema(many=True)
