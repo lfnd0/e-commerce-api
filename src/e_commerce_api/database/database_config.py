4fa39a82a94a2ff0019cb254d1ee59ef0b54d924
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-07-10 20:38:56 -0300

    refactor: modify import order

--- src/e_commerce_api/database/database_config.py
+++ src/e_commerce_api/database/database_config.py
@@ -1,6 +1,6 @@
-from e_commerce_api import app
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
+from e_commerce_api import app
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///ecommerce.db"
 
