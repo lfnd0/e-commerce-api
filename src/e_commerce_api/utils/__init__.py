d3281ca0d66ae6cd8f97e861c76007abb6737ace
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-07-09 21:27:11 -0300

    feat: add encryption class

--- src/e_commerce_api/utils/__init__.py
+++ src/e_commerce_api/utils/__init__.py
@@ -1,5 +1,1 @@
-def encode_password(password: str):
-    return password.encode("utf-8")
-
-def decode_password(password: bytes):
-    return password.decode("utf-8")
+from .encryption import Encryption
