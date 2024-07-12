efff63c26380a543d8421b0a8ea325abbb8ce44e
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-07-11 23:43:56 -0300

    refactor: rename product capture factory by id

--- src/e_commerce_api/use_cases/products/factories/__init__.py
+++ src/e_commerce_api/use_cases/products/factories/__init__.py
@@ -1,5 +1,7 @@
 from .make_create_product_use_case import make_create_product_use_case
 from .make_list_products_use_case import make_list_products_use_case
-from .make_fetch_product_by_id_use_case import make_fetch_product_by_id_use_case
+from .make_fetch_product_by_id_with_schema_use_case import (
+    make_fetch_product_by_id_with_schema_use_case,
+)
 from .make_remove_product_by_id_use_case import make_remove_product_by_id_use_case
 from .make_update_product_by_id_use_case import make_update_product_by_id_use_case
