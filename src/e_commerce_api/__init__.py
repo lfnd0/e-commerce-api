8a735eff843529e528e59842b0b3a653757f8705
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-07-15 21:44:50 -0300

    feat: add app module

--- src/e_commerce_api/__init__.py
+++ src/e_commerce_api/__init__.py
@@ -1,7 +1,3 @@
-from flask import Blueprint
-from .app import app, main, set_blueprint
-from .database import db
-from .routes import product_blueprint, get_root, post_product, get_products, get_product_by_id, delete_product_by_id, patch_product_by_id
-from .models import Product
-
-app.register_blueprint(product_blueprint, url_prefix='/api/products')
+from .app import *
+from .models import *
+from .routes import *
--- src/e_commerce_api/app.py
+++ src/e_commerce_api/app.py
@@ -1,9 +1,40 @@
-from flask import Flask, Blueprint
+import os
+from flask import Flask
+from flask_cors import CORS
+from .database import DatabaseConfig
 
-app = Flask(__name__)
 
-def set_blueprint(blueprint_name: str):
-    return Blueprint(blueprint_name, __name__)
+class _App:
 
-def main():
-    app.run(debug=True, host="0.0.0.0")
+    def __init__(self):
+        self._secret_key = os.getenv("SECRET_KEY")
+        self._database_uri = os.getenv("DATABASE_URI")
+        self._host = os.getenv("HOST")
+
+        self._app = Flask(__name__)
+        self._app.config["SECRET_KEY"] = self._secret_key
+
+        self._database_config = DatabaseConfig(self._app, self._database_uri)
+        self._db = self._database_config.get_db()
+        self._ma = self._database_config.get_ma()
+
+        CORS(self._app)
+
+    def get_app(self):
+        return self._app
+
+    def get_db(self):
+        return self._db
+
+    def get_ma(self):
+        return self._ma
+
+    def run(self):
+        self._app.run(debug=True, host=self._host)
+
+
+app_module = _App()
+
+app = app_module.get_app()
+db = app_module.get_db()
+ma = app_module.get_ma()
