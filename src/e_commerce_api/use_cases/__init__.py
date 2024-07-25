e10c453ce6689d7ba134668cf8100e8755dd187a
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-07-24 21:55:56 -0300

    feat: add import from user factories

--- src/e_commerce_api/use_cases/__init__.py
+++ src/e_commerce_api/use_cases/__init__.py
@@ -1,1 +1,8 @@
-from .products.factories import make_create_product_use_case, make_list_products_use_case, make_fetch_product_by_id_use_case, make_remove_product_by_id_use_case, make_update_product_by_id_use_case
+from .products.factories import (
+    make_create_product_use_case,
+    make_list_products_use_case,
+    make_fetch_product_by_id_with_schema_use_case,
+    make_remove_product_by_id_use_case,
+    make_update_product_by_id_use_case,
+)
+from .users.factories import make_create_user_use_case, make_authenticate_user_use_case
