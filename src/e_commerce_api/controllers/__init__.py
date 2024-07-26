8c6ac2f7ea7358404d7c05dc8f04cc3bc35bfcb2
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-07-25 22:32:35 -0300

    feat: add user controllers

--- src/e_commerce_api/controllers/__init__.py
+++ src/e_commerce_api/controllers/__init__.py
@@ -1,2 +1,9 @@
 from .root_controller import read_root
-from .product_controller import create_product, list_products, fetch_product_by_id, remove_product_by_id, update_product_by_id
+from .product_controller import (
+    create_product,
+    list_products,
+    fetch_product_by_id_with_schema,
+    remove_product_by_id,
+    update_product_by_id,
+)
+from .user_controller import create_user, authenticate_user, revoke_user_authentication
