af24bb69bcf88f51e656dcc8af7719f8d40c2d96
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-07-26 22:53:35 -0300

    feat: add user routes

--- src/e_commerce_api/routes/__init__.py
+++ src/e_commerce_api/routes/__init__.py
@@ -1,2 +1,10 @@
 from .root_route import get_root
-from .product_route import product_blueprint, post_product, get_products, get_product_by_id, delete_product_by_id, patch_product_by_id
+from .product_route import (
+    product_blueprint,
+    post_product,
+    get_products,
+    get_product_by_id,
+    delete_product_by_id,
+    patch_product_by_id,
+)
+from .user_route import user_blueprint, post_user, login, logout
