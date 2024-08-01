5e7b0476797fdafffcabca0860d918a2a3495a85
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-07-31 23:08:27 -0300

    refactor: remove product by id

--- src/e_commerce_api/use_cases/products/remove_product_by_id_use_case.py
+++ src/e_commerce_api/use_cases/products/remove_product_by_id_use_case.py
@@ -1,10 +1,10 @@
 from ...repositories import ProductRepository
 
+
 class RemoveProductByIdUseCase:
 
     def __init__(self, products_repository: ProductRepository):
         self.products_repository = products_repository
 
     def execute(self, product_id: int):
-        product = self.products_repository.fetch_product_by_id_no_schema(product_id)
-        return self.products_repository.remove_product_by_id(product)
+        self.products_repository.remove_product_by_id(product_id)
