84d768c37e7d0e08ef020eeb63b0e4dbe0feae87
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-08-09 22:35:01 -0300

    refactor: change cart model

--- src/e_commerce_api/models/cart.py
+++ src/e_commerce_api/models/cart.py
@@ -1,30 +1,35 @@
 from datetime import datetime
 from e_commerce_api import db, ma
-from .product import ProductSchema
-
-cart_product = db.Table(
-    "cart_product",
-    db.Column("cart_id", db.Integer, db.ForeignKey("cart.id"), primary_key=True),
-    db.Column("product_id", db.Integer, db.ForeignKey("product.id"), primary_key=True),
-    db.Column("product_quantity", db.Integer, nullable=False),
-)
+from .user import UserSchema
+from .product_in_cart import ProductInCartSchema
 
 
 class Cart(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    total = db.Column(db.Float, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.now())
     updated_at = db.Column(db.DateTime, nullable=True)
+    client_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
 
-    products = db.relationship(
-        "Product", secondary=cart_product, backref=db.backref("carts", lazy="select")
+    client = db.relationship("User", backref=db.backref("carts", lazy="select"))
+    products_in_cart = db.relationship(
+        "ProductInCart", backref=db.backref("cart", lazy="select")
     )
 
 
 class CartSchema(ma.Schema):
-    products = ma.Nested(ProductSchema)
+    client = ma.Nested(UserSchema)
+    products_in_cart = ma.List(ma.Nested(ProductInCartSchema))
 
     class Meta:
-        fields = ("id", "created_at", "updated_at", "products")
+        fields = (
+            "id",
+            "total",
+            "created_at",
+            "updated_at",
+            "client",
+            "products_in_cart",
+        )
 
 
 cart_schema = CartSchema()
