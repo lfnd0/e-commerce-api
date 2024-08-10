e29b01e8702116d40a4168ad55a7e1ac4674b87c
luiz.fernando <luiz-fsilva37@hotmail.com>
2024-08-09 22:36:32 -0300

    feat: add product in cart model

--- src/e_commerce_api/models/__init__.py
+++ src/e_commerce_api/models/__init__.py
@@ -1,3 +1,9 @@
 from .product import Product, ProductSchema, product_schema, products_schema
 from .user import User, UserSchema, user_schema, users_schema
 from .cart import Cart, CartSchema, cart_schema, carts_schema
+from .product_in_cart import (
+    ProductInCart,
+    ProductInCartSchema,
+    product_in_cart_schema,
+    product_in_carts_schema,
+)
